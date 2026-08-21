052ddb76b1dd4d26f76dc76bd3be8a10ed814f23
search-first <lmserpteam@gmail.com>
2026-08-22 02:07:37 +0530

    first commit

--- server/server.js
+++ server/server.js
@@ -1,14 +1,10 @@
 import express from "express";
 
 const app = express();
-const PORT = 5000;
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.json({ message: "Skillora API is running" });
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
 });
